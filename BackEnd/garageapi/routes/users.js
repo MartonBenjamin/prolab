36b984ef8b05aa66750593f823c760730352317b
MartonBenjamin <emben97@gmail.com>
2020-04-01 15:29:07 +0200

    Regisztráció implementálva
    Refaktort igényel.
    specifikáció szerinti jsonből működik

--- BackEnd/garageapi/routes/users.js
+++ BackEnd/garageapi/routes/users.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
+var passwordHash = require('password-hash');
 
 var con = mysql.createConnection({
   host: "localhost",
@@ -11,7 +12,7 @@ var con = mysql.createConnection({
 
 con.connect(function(err) {
   if (err) throw err;
-  console.log("Connected!");
+  console.log("Successfully connected to garageDoor database!");
 });
 
 /* GET users listing. */
@@ -26,6 +27,36 @@ router.get('/add', function(req, res, next) {
     if (err) throw err;
     console.log("1 record inserted");
   });
+});
+router.post('/register',function (req,res) {
+  try {
+    const hashedPassword=passwordHash.generate(req.body.password); //Jelszó hashelése
+    var values = [
+      ['username', req.body.username],
+      ['password', hashedPassword],
+      ['email', req.body.email],
+      ['first_name', req.body.first_name],
+      ['last_name', req.body.last_name],
+      ['created_at', '2012-12-12']
+    ];
+    console.log(values);
+    var sql = "INSERT INTO users (username, password, email, firstname, lastname, created_at) VALUES ('"+req.body.username+"','"+hashedPassword+"','"+req.body.email+"','"+req.body.first_name+"','"+req.body.last_name+"','2018-12-12')";
+
+    con.query(sql,[values],function(err,result){
+        if (err) return console.log(err);
+      console.log("Successfully registered");
+    });
+    res.status(201).send();
+    console.log(user.username);
+  }
+  catch
+  {
+    res.status(500).send();
+  }
+});
+
+router.get('/listuser',function(req,res){
+  console.log(users);
 });
 
 module.exports = router;
