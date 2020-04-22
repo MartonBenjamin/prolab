56bca4b501d28c942603862dde94025770cf2dec
Sass-Gyarmati Norbert <sassnorbert99@gmail.com>
2020-04-22 09:27:05 +0200

    About.js létrehozva, melyben részletesen levan írva a szoftver funkciója

---
+++ test/src/About.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './App.css';
+import './LeftBar';
+import './RightBar';
+import LeftBar from './LeftBar';
+import RightBar from './RightBar';
+
+
+
+function About() {
+    return (
+
+        <div>
+            <p></p>
+            <h1> US </h1>
+
+
+            <LeftBar/>
+            <RightBar/>
+
+            <div className="center"> <ul>
+                Garage Door Designs
+                When building a new garage there are many different aspects that you will need to consider. The garage door company that you are getting your garage from can answer many of these questions so you will have the perfect garage. Planning a new garage calls for detailed planning and is not something that can be rushed into. One important thing that a garage door company will need to know is whether it will be an attached or detached garage. With an attached garage, it is very convenient during wet and cold weather. It is a popular choice for homeowners because you pull into the garage and go straight into the house.
+
+                If you have the extra space and do not mind going out into the weather to get to the house choose a detached garage. The extra space between the house and garage can be used for a patio or even a kitchen garden. From the garage to the home you could also build a nice aesthetically walkway. You will also need to decide if you want a one or two car garage and if you need extra room for storage. If you choose a simple one-car garage, a door company states that you will need a basic space of 12 feet by 24 feet. If you choose a two-car garage you will need to add another 9 feet by 18 feet of space for that extra car. You will need this much space so you can open the doors without scratching or damaging the car doors.
+
+
+                Features of the garage
+
+                There are certain things according the door company that you should consider having for the interior of your new garage. Some of the common elements of a modern garage design include a sensor system and automated doors. It is suggested that you go with an automated door system. With this system, the door will automatically open when your car gets near the door. This design will save you getting out of your car and opening the doors. Today, some people use a garage for a playing, living, and storage area instead of a place to park the car. It can be a "man-cave", a gym, a playroom, and more.
+
+                A garage door company suggests that when building a garage you should consider safety by installing one or more surveillance cameras. They can be used to monitor the interior of the garage and the surrounding areas. Take your time and talk to the garage door company for their opinions and suggestions for your new garage. Before you start to design your new garage be sure that you look at your budget because you do not want to run out of money and have a half-finished garage.
+
+            </ul></div>
+        </div>
+
+    );
+};
+
+
+export default About;
