3b7bbf6ee3b29492b2e3230247ca0ba1437a5e46
Sass-Gyarmati Norbert <sassnorbert99@gmail.com>
2020-04-22 09:22:18 +0200

    App.js-ben importálások létrehozva

--- test/src/App.js
+++ test/src/App.js
@@ -2,18 +2,59 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import React from 'react';
+import logo from './logo.svg';
+import { BrowserRouter,Switch ,Route,Link } from "react-router-dom";
+import Shop from './Shop';
+
+import Home from './Home';
+import Nav2 from './Nav2';
+import SignIn from './Singin';
+import SignUp from './SingUp';
+
+import Infobutton from './Infobutton';
+import './App.css';
+import RightBar from './RightBar';
+
+import About from './About';
+import Nav from './Nav';
+
+
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="overlay">
+            <BrowserRouter>
+                <div className="App">
+
+
+
+                    <Nav></Nav>
+
+                    <Switch>
+
+                        <Route path="/Nav2" exact component={Nav2} />
+
+                        <Route path="/" exact component={Home} />
+                        <Route path="/About" exact component={About} />
+
+                        <Route path="/shop" exact component={Shop} />
+                        <Route path="/SingIn" exact component={SignIn} />
+                        <Route path="/SingUp" exact component={SignUp} />
+
+                    </Switch>
+
+
+
+
+
+
+
+                </div>
+
+            </BrowserRouter>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
     );
   }
 }
