18e502ddfb8f9bd14bd86e97416c27c12ded0652
Sass-Gyarmati Norbert <sassnorbert99@gmail.com>
2020-04-27 19:39:20 +0200

    GarageDoorController.js fejlesztve
    További funkciókat írtam a fent említett js kiterjesztésű fájlhoz, melyet @martonbenjamin-al fejlesztettünk.
    #25

--- BackEnd/GarageAPP/controllers/garagedoorcontroller.js
+++ BackEnd/GarageAPP/controllers/garagedoorcontroller.js
@@ -18,4 +18,56 @@ module.exports= {
             });
         });
     },
+
+    getDoorById: (req, res) => {
+        const id = req.params.id;
+        getDoorById(id, (err, results) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            if (!results) {
+                return res.json({
+                    message: "There is no garage door with the ID:"+id
+                });
+            }
+            return res.json({
+                succes: 1,
+                data: results
+            });
+        });
+    },
+
+    getDoors: (req, res) => {
+        getDoors((err, results) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            return res.json({
+                success: 1,
+                data: results
+            });
+        });
+    },
+
+    deleteDoor: (req, res) => {
+        const data = req.body;
+        deleteDoor(data, (err, results) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            if (!results) {
+                return res.json({
+                    success: 0,
+                    message: "Nincs ilyen id-jű garázs ajtó."
+                });
+            }
+            return res.json({
+                success: 1,
+                message: "Garázs ajtó sikeresen törölve."
+            });
+        });
+    },
 }
